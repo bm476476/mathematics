9135bb6edb19cbb07442781678bbc6f914cd69cc
bm476476 <152660253+bm476476@users.noreply.github.com>
2024-01-05 15:35:06 -0500

    Update primes.py
    Updated with start and limit function as well as updated amountOfprimes variable

--- primes.py
+++ primes.py
@@ -1,20 +1,27 @@
+
+limit = int(input("Type in the value you don't want to exceed: "))
+start = int(input("Type in the value you want to start at (type in 0 if no value is wanted): "))
+amountOfPrimes = int(input("Type in the amount of prime numbers you want to print: "))
+numberPrint = []
+
 def is_prime(n):
-  if n <= 1:
-    return False
+  """Returns True if n is a prime number, False otherwise."""
   for i in range(2, int(n**0.5) + 1):
     if n % i == 0:
       return False
+  numberPrint.append(n)
   return True
 
-def get_prime_numbers(n):
-  primes = []
-  for i in range(2, n + 1):
-    if is_prime(i):
-      primes.append(i)
-  return primes
+for i in range(start, limit):
+  if start == 0:
+    break  
+  is_prime(i)
 
-# Example usage
-n = int(input("How many prime numbers do you want: "))
+for i in range (0, 10000000):
+  is_prime(i)
+  if len(numberPrint) == amountOfPrimes:
+      break
 
-prime_numbers = get_prime_numbers(n)
-print(f"The first {n} prime numbers are: {prime_numbers}")
+print(numberPrint)
+
+  
